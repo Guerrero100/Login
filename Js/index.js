2368a85fa1fc1274cc7922c08137a87a3e5590fd
JulianGuerrero <guerreroydragonxxs@gmail.com>
2022-11-22 22:27:36 -0500

    Fase JavaScript con Expresiones regules

--- Js/index.js
+++ Js/index.js
@@ -1,3 +1,25 @@
+let username = document.getElementById('username');
+    let password = document.getElementById('password');
+    let email = document.getElementById('email');
+    
+
+const Expresiones = {
+    usuario: /^[a-zA-Z0-9\_\-]{4,16}$/, // Letras, numeros, guion y guion_bajo
+	nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
+	password: /^.{4,12}$/, // 4 a 12 digitos.
+	correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,// Letras, guion bajo caracteres especiales. 
+	telefono: /^\d{7,14}$/ // 7 a 14 numeros.
+}
+
+
+
+const ValidarCampos = e =>{
+
+    
+
+}
+
+
 function setFormMessage(formElement, type, message) {
     const messageElement = formElement.querySelector(".form__message");
 
@@ -6,9 +28,9 @@ function setFormMessage(formElement, type, message) {
     messageElement.classList.add(`form__message--${type}`);
 }
 
-function setInputError(inputElement, mensaje) {
+function setInputError(inputElement, message) {
     inputElement.classList.add("form__input--error");
-    inputElement.parentElement.querySelector(".form__input-error-message").textContent = mensaje;
+    inputElement.parentElement.querySelector(".form__input-error-message").textContent = message;
 }
 
 function clearInputError(inputElement) {
@@ -37,13 +59,13 @@ document.addEventListener("DOMContentLoaded", () => {
 
        
 
-        setFormMessage(loginForm, "error", "Usuario o Contraseña invalida o incorrecta");
+        setFormMessage(loginForm, "error", "Usuario invalidado o contraseña incorrecta.");
     });
 
     document.querySelectorAll(".form__input").forEach(inputElement => {
         inputElement.addEventListener("blur", e => {
             if (e.target.id === "signupUsername" && e.target.value.length > 0 && e.target.value.length < 10) {
-                setInputError(inputElement, "El usuario debe tener como minimo 10 caracteres");
+                setInputError(inputElement, "El usuario ha pasado los 10 caracteres.");
             }
         });
 
