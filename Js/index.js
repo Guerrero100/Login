049bae4da1ae7259384e5d22e873c0ed49f436ed
JulianGuerrero <guerreroydragonxxs@gmail.com>
2023-02-23 19:03:34 -0500

    Login

--- Js/index.js
+++ Js/index.js
@@ -4,15 +4,39 @@
     let formulario = document.getElementById('formulario');
     let password = document.getElementById('password');
     let email = document.getElementById('email');
+    const inputs = document.querySelectorAll('#formulario input');
     
 
 const Expresiones = {
     usuario: /^[a-zA-Z0-9\_\-]{4,16}$/, // Letras, numeros, guion y guion_bajo
-	nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
 	password: /^.{4,12}$/, // 4 a 12 digitos.
 	correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,// Letras, guion bajo caracteres especiales. 
-	telefono: /^\d{7,14}$/ // 7 a 14 numeros.
 }
+
+const campos ={
+    usuario:false,
+    password:false,
+    correo:false 
+}
+
+const validarFormulario = (e) => {
+   switch(e.target.name){
+    case "usuario":
+            validarFormulario(Expresiones.usuario,e.target,'usuario');
+    break;
+    case "correo":
+            validarCampo(Expresiones.correo, e.target, 'correo');
+    break;
+    case "password":
+            validarCampo(expresiones.password, e.target, 'password');
+            validarpassword2();
+    break;
+    case "password2":
+    |       validarpassword2();
+    break;
+   }
+    
+};
 
 
 function setFormMessage(formElement, type, message) {
@@ -34,34 +58,31 @@ function clearInputError(inputElement) {
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-    const loginForm = document.querySelector("#login");
+    const formulario = document.querySelector("#login");
     const createAccountForm = document.querySelector("#createAccount");
 
     document.querySelector("#linkCreateAccount").addEventListener("click", e => {
         e.preventDefault();
-        loginForm.classList.add("formulario-hidden");
+        formulario.classList.add("formulario-hidden");
         createAccountForm.classList.remove("formulario-hidden");
     });
 
     document.querySelector("#linkLogin").addEventListener("click", e => {
         e.preventDefault();
         createAccountForm.classList.add("formulario-hidden");
-        loginForm.classList.remove("formulario-hidden");
+        formulario.classList.remove("formulario-hidden");
     });
 
-    loginForm.addEventListener("submit", e => {
+    formulario.addEventListener("submit", (e) => {
         e.preventDefault();
 
         // Perform your AJAX/Fetch login
-        setFormMessage(loginForm, "error", "Usuario invalidado o contraseña incorrecta.");
+        setFormMessage(formulario, "error", `Usuario incorrecto y/o contraseña incorrecta.`);
     });
 
-    document.querySelectorAll(".formulario-input").forEach(inputElement => {
-        inputElement.addEventListener("blur", e => {
-            if (e.target.id === "IngresarUsuario" && e.target.value.length > 0 && e.target.value.length < 10) {
-                setInputError(inputElement, "El usuario debe tener minimo los 10 caracteres.");
-            }
-        });
+    inputs.forEach(inputElement => {
+        inputElement.addEventListener("blur", validarFormulario);
+        inputElement.addEventListener("keyup", validarFormulario);
 
         inputElement.addEventListener("input", e => {
             clearInputError(inputElement);
